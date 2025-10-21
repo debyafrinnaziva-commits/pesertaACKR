9d816c86e3f59d1e4f93650a82b3d10031ee0518
debyafrinnaziva-commits <debyafrinnaziva@gmail.com>
2025-10-21 13:28:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@
 // 2. Tambahkan 'event listener' untuk aksi 'click'
   tombolSapa.addEventListener('click', function() {
 // 3. Jalankan kode ini ketika tombol di-klik                            
-   alert('Hallo! Terimakasih sudah berkunjung!);
+   alert('Hallo! Terimakasih sudah berkunjung!');
 });
